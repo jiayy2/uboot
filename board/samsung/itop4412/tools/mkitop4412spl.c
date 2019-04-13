1edbc84876abf354cd3f38a8c4ed12531322a090
simexce <simexce@163.com>
2019-04-13 22:39:36 +0800

    feat: 制作启动镜像
    镜像布局：
        BL1     8K      0x0
        BL2     16K     0x2000
        UBOOT   xxx     0x6000
    
    测试信息：
    1.计算布局与生成镜像的布局完全相符

--- board/samsung/itop4412/tools/mkitop4412spl.c
+++ board/samsung/itop4412/tools/mkitop4412spl.c
@@ -1,109 +1,65 @@
-// SPDX-License-Identifier: GPL-2.0+
-/*
- * Copyright (C) 2011 Samsung Electronics
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/stat.h>
 
-#define BUFSIZE			(16*1024)
-#define IMG_SIZE		(16*1024)
-#define SPL_HEADER_SIZE		16
-#define FILE_PERM		(S_IRUSR | S_IWUSR | S_IRGRP \
-				| S_IWGRP | S_IROTH | S_IWOTH)
-#define SPL_HEADER		"S5PC210 HEADER  "
-/*
-* Requirement:
-* IROM code reads first 14K bytes from boot device.
-* It then calculates the checksum of 14K-4 bytes and compare with data at
-* 14K-4 offset.
-*
-* This function takes two filenames:
-* IN  "u-boot-spl.bin" and
-* OUT "$(BOARD)-spl.bin as filenames.
-* It reads the "u-boot-spl.bin" in 16K buffer.
-* It calculates checksum of 14K-4 Bytes and stores at 14K-4 offset in buffer.
-* It writes the buffer to "$(BOARD)-spl.bin" file.
-*/
+#define BL2_SZ 14332
+#define BAC_SZ 16384
 
-int main(int argc, char **argv)
+static int checksum(char* fn_src,char* fn_dst)
 {
-	int i, len;
-	unsigned char buffer[BUFSIZE] = {0};
-	int ifd, ofd;
-	unsigned int checksum = 0, count;
+    FILE *fp_src = NULL;
+    FILE *fp_dst = NULL;
 
-	if (argc != 3) {
-		printf(" %d Wrong number of arguments\n", argc);
-		exit(EXIT_FAILURE);
-	}
+    unsigned char *bytes = NULL;
+    int size = 0;
+    int i = 0;
 
-	ifd = open(argv[1], O_RDONLY);
-	if (ifd < 0) {
-		fprintf(stderr, "%s: Can't open %s: %s\n",
-			argv[0], argv[1], strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+    fp_src = fopen(fn_src, "rb");
+    if (NULL == fp_src)
+    {
+        printf("can't open file for read(%s)\n", fn_src);
+        return -1;
+    }
 
-	ofd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, FILE_PERM);
-	if (ofd < 0) {
-		fprintf(stderr, "%s: Can't open %s: %s\n",
-			argv[0], argv[2], strerror(errno));
-		if (ifd)
-			close(ifd);
-		exit(EXIT_FAILURE);
-	}
+    fp_dst = fopen(fn_dst, "wb");
+    if (NULL == fp_dst)
+    {
+        printf("can't open file for read(%s)\n", fn_dst);
+        return -1;
+    }
 
-	len = lseek(ifd, 0, SEEK_END);
-	lseek(ifd, 0, SEEK_SET);
+    fseek(fp_src, 0L, SEEK_END);
+    size = ftell(fp_src);
+    size = (size > BL2_SZ) ? BL2_SZ : size;
 
-	memcpy(&buffer[0], SPL_HEADER, SPL_HEADER_SIZE);
+    bytes = (unsigned char *)calloc(BAC_SZ, 1);
+    rewind(fp_src);
+    fread(bytes, 1, size, fp_src);
 
-	count = (len < (IMG_SIZE - SPL_HEADER_SIZE))
-		? len : (IMG_SIZE - SPL_HEADER_SIZE);
+    unsigned int sum = 0;
+    for(i = 0;i < size;i++)
+    {
+        sum += bytes[i];
+    }
 
-	if (read(ifd, buffer + SPL_HEADER_SIZE, count) != count) {
-		fprintf(stderr, "%s: Can't read %s: %s\n",
-			argv[0], argv[1], strerror(errno));
+    *(unsigned int*)(bytes+BL2_SZ) = sum;
 
-		if (ifd)
-			close(ifd);
-		if (ofd)
-			close(ofd);
+    fwrite(bytes, 1, BAC_SZ, fp_dst);
 
-		exit(EXIT_FAILURE);
-	}
+    free(bytes);
+    fclose(fp_src);
+    fclose(fp_dst);
 
-	for (i = 0; i < IMG_SIZE - SPL_HEADER_SIZE; i++)
-		checksum += buffer[i+16];
+    return 0;
+}
 
-	*(unsigned long *)buffer ^= 0x1f;
-	*(unsigned long *)(buffer+4) ^= checksum;
 
-	for (i = 1; i < SPL_HEADER_SIZE; i++)
-		buffer[i] ^= buffer[i-1];
-
-	if (write(ofd, buffer, BUFSIZE) != BUFSIZE) {
-		fprintf(stderr, "%s: Can't write %s: %s\n",
-			argv[0], argv[2], strerror(errno));
-
-		if (ifd)
-			close(ifd);
-		if (ofd)
-			close(ofd);
-
-		exit(EXIT_FAILURE);
-	}
-
-	if (ifd)
-		close(ifd);
-	if (ofd)
-		close(ofd);
-
-	return EXIT_SUCCESS;
+int main(int argc, char *argv[])
+{
+    if(argc == 3){
+        return checksum(argv[1],argv[2]);
+    }else{
+        printf("Format: Program_Name Source_File Target_File\n");
+    }
+    return -1;
 }
